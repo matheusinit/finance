4542df1611cef18c4d24818fd6088d13fa25df23
suetham <matheus.oliveira.s@protonmail.com>
2024-03-30 13:34:55 -0300

    feat: ensure password uses the standard of strong password - closes #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 10 },
+            format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_])/,
+               message: "must contain at least one lowercase letter, one uppercase letter, one special character, and one number" }
 end
