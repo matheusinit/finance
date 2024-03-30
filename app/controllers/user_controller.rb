30b640d20e863093a2e1c8ffa57cdfd30e5cede4
suetham <matheus.oliveira.s@protonmail.com>
2024-03-29 21:46:49 -0300

    refactor: remove logger output for development

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,5 @@
 class UserController < ApplicationController
   def create
-    logger.debug "name: #{params[:name]}"
-    logger.debug "email: #{params[:email]}"
-    logger.debug "password: #{params[:password]}"
-
     user = User.new
     user.id = SecureRandom.uuid
     user.name = params[:name]
