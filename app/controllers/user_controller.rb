b3f8c43bd8792a759bbaf84375eda3dbbfc4d69c
Matheus <matheus.oliveira.s@protonmail.com>
2024-04-07 18:43:09 -0300

    fix: ensure error message is shown when login_attempts reach to 3

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -48,27 +48,28 @@ class UserController < ApplicationController
 
     user = User.find_by(email: params[:email])
 
-    account = Account.new
-    
+    account = Account.new 
+
     if user
       account = Account.find_by(user_id: user.id)
-    end
-
-    if account && account.login_attempts >= 3 and account.updated_at > 10.minutes.ago
-      logger.debug "Too many login attempts: #{account}"
-      redirect_to '/login', alert: 'Muitas tentativas de login, o seu usuario foi bloqueado por 10 minutos. Tente novamente em breve.'
-      return
-    end
-
-    if params[:password] and account and account.updated_at <= 10.minutes.ago
-      account.login_attempts = 0
-      account.save
     end
 
     if user && user.authenticate(params[:password])
       redirect_to '/'
     elsif not is_blank_email and not is_blank_password 
+      if account and account.updated_at <= 10.minutes.ago
+        reset_login_attempts(user)
+      end
+
       increase_login_attemps(user)
+      login_is_blocked = is_login_blocked?(user)
+
+      if login_is_blocked
+        logger.debug "Too many login attempts: #{account}"
+        redirect_to '/login', alert: 'Muitas tentativas de login, o seu usuario foi bloqueado por 10 minutos. Tente novamente em breve.'
+        return
+      end
+
       redirect_to '/login', alert: 'Credenciais inválidas'
     end
   end
@@ -82,6 +83,33 @@ class UserController < ApplicationController
 
     account = Account.find_by(user_id: user.id)
     account.increment!(:login_attempts)
-    account.touch
+    if account.login_attempts <= 3
+      account.touch
+    end
+  end
+
+  def is_login_blocked?(user)
+    unless user.is_a?(User)
+      raise ArgumentError.new("Object user must be of Class User")
+    end
+
+    account = Account.find_by(user_id: user.id)
+    account.updated_at > 10.minutes.ago
+
+    if account && account.login_attempts >= 3 and account.updated_at > 10.minutes.ago
+      return true
+    end
+
+    return false
+  end
+
+  def reset_login_attempts(user)
+    unless user.is_a?(User)
+      raise ArgumentError.new("Object user must be of Class User")
+    end
+
+    account = Account.find_by(user_id: user.id)
+    account.login_attempts = 0
+    account.save
   end
 end
