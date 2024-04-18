74ab0c47a9a3b7f63d4473109e6f281e5e2fcba0
Matheus <matheus.oliveira.s@protonmail.com>
2024-04-18 11:16:12 -0300

    feat: build the initial view of the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/", to: "page#home"
+
   get "/user/new", to: "user#create", as: :signup
   get "/login", to: "user#login", as: :login
   get "/logout", to: "user#destroy_session"
