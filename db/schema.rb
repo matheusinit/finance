ae575694001468c7c0a427824232719115f0695d
matheusinit <matheus.oliveira.s@protonmail.com>
2024-05-22 18:05:58 -0300

    chore: configure PostgreSQL connection

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_122920) do
-# Could not dump table "accounts" because of following StandardError
-#   Unknown type 'uuid' for column 'id'
+ActiveRecord::Schema[7.1].define(version: 2024_05_02_211655) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
-# Could not dump table "incomes" because of following StandardError
-#   Unknown type 'uuid' for column 'id'
+  create_table "accounts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "login_attempts", default: 0
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
-# Could not dump table "receipts" because of following StandardError
-#   Unknown type 'uuid' for column 'id'
+  create_table "expenses", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "value", null: false
+    t.boolean "is_paid", default: false, null: false
+    t.integer "payment_day"
+    t.integer "installments_number", default: 1, null: false
+    t.uuid "receipt_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receipt_id"], name: "index_expenses_on_receipt_id"
+  end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type '' for column 'id'
+  create_table "incomes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "value", null: false
+    t.boolean "is_monthly_recurring", default: false, null: false
+    t.integer "payment_day"
+    t.uuid "receipt_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_paid", default: false, null: false
+    t.index ["receipt_id"], name: "index_incomes_on_receipt_id"
+  end
+
+  create_table "receipts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.uuid "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["user_id"], name: "index_receipts_on_user_id"
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+  end
 
   add_foreign_key "accounts", "users"
   add_foreign_key "expenses", "receipts"
