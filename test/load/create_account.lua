d6311a49c7a8e2422db88f2a6fba9bb16935e947
Matheus <matheus.oliveira.s@protonmail.com>
2024-05-28 22:59:03 -0300

    chore: uncomment necessary code for load testing

--- test/load/create_account.lua
+++ test/load/create_account.lua
@@ -8,16 +8,16 @@ end
 response = function(status, headers, body)
 	if status == 200 and body then
 		-- Extract CSRF token from the meta tag in the HTML body
-		-- local token = string.match(body, '<meta name="csrf%-token" content="(.-)" />')
-		-- if token then
-		-- Update wrk method to POST for the next request
-		wrk.method = "POST"
-		wrk.body = "name=TestUser&email=test"
-			.. math.random(100000)
-			.. "@example.com&password=Password1.&password_confirmation=Password1."
-		-- wrk.headers["X-CSRF-Token"] = token
-		wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
-		-- end
+		local token = string.match(body, '<meta name="csrf%-token" content="(.-)" />')
+		if token then
+			-- Update wrk method to POST for the next request
+			wrk.method = "POST"
+			wrk.body = "name=TestUser&email=test"
+				.. math.random(100000)
+				.. "@example.com&password=Password1.&password_confirmation=Password1."
+			wrk.headers["X-CSRF-Token"] = token
+			wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
+		end
 	end
 end
 
