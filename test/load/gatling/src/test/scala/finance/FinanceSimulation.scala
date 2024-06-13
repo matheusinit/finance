b33cf867abd7154715b2d9970a9dad7a33c92b90
matheusinit <matheus.oliveira.s@protonmail.com>
2024-06-13 13:26:30 -0300

    refactor: break requests of creation of accounts scenario

--- test/load/gatling/src/test/scala/finance/FinanceSimulation.scala
+++ test/load/gatling/src/test/scala/finance/FinanceSimulation.scala
@@ -21,26 +21,26 @@ class FinanceSimulation extends Simulation {
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"
     )
 
+  val getCSRFToken = http("Get CSRF Token")
+    .get("/user/new")
+    .check(status.is(200))
+    .check(
+      css("meta[name='csrf-token']", "content").saveAs("csrfToken")
+    )
+
+  val createUser = http("Create user")
+    .post("/user/new")
+    .header("x-csrf-token", "#{csrfToken}")
+    .header("content-type", "application/x-www-form-urlencoded")
+    .formParam("name", "Matheus Oliveira")
+    .formParam("email", "matheus13@email.com")
+    .formParam("password", "Pandaninja13.")
+    .formParam("password_confirmation", "Pandaninja13.")
+    .check(status.in(201))
+
   val accountsCreation = scenario("Creation of accounts")
-    .exec(
-      http("Get CSRF Token")
-        .get("/user/new")
-        .check(status.is(200))
-        .check(
-          css("meta[name='csrf-token']", "content").saveAs("csrfToken")
-        )
-    )
-    .exec(
-      http("Create user")
-        .post("/user/new")
-        .header("x-csrf-token", "#{csrfToken}")
-        .header("content-type", "application/x-www-form-urlencoded")
-        .formParam("name", "Matheus Oliveira")
-        .formParam("email", "matheus13@email.com")
-        .formParam("password", "Pandaninja13.")
-        .formParam("password_confirmation", "Pandaninja13.")
-        .check(status.in(201, 422, 400, 500))
-    )
+    .exec(getCSRFToken)
+    .exec(createUser)
     .pause(1.milliseconds, 30.milliseconds)
 
   setUp(
