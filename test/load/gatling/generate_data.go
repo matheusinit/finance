ea7b4476e7ad3a4dbe5161431ec90d482a6cd5d5
Matheus <matheus.oliveira.s@protonmail.com>
2024-06-18 10:43:36 -0300

    feat: ensure all passwords has uppercase and lowercase letters

--- test/load/gatling/generate_data.go
+++ test/load/gatling/generate_data.go
@@ -3,12 +3,54 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"github.com/brianvoe/gofakeit/v7"
 	"os"
+	"strings"
+
+	// "strings"
+
+	"github.com/brianvoe/gofakeit/v7"
 )
 
+func force_password_pattern(password string) string {
+	has_uppercase_letter := false
+	has_lowercase_letter := false
+
+	var first_uppercase_letter byte
+	var first_lowercase_letter byte
+
+	for i := 0; i < len(password); i++ {
+		letter := password[i]
+
+		if letter >= 65 && letter <= 90 {
+			has_uppercase_letter = true
+
+			first_uppercase_letter = letter
+		}
+
+		if letter >= 97 && letter <= 122 {
+			has_lowercase_letter = true
+			first_lowercase_letter = letter
+		}
+	}
+
+	if has_lowercase_letter == false || has_uppercase_letter == false {
+		fmt.Printf("before: %s\n", password)
+		fmt.Printf("after: %s\n", password)
+	}
+
+	if has_lowercase_letter == false {
+		password = strings.Replace(password, string(first_uppercase_letter), string(first_uppercase_letter+32), 1)
+	}
+
+	if has_uppercase_letter == false {
+		password = strings.Replace(password, string(first_lowercase_letter), string(first_lowercase_letter-32), 1)
+	}
+
+	return password
+}
+
 func main() {
-	const NUMBER_OF_RECORDS = 1000000
+	const NUMBER_OF_RECORDS = 10
 
 	// var csv_content strings.Builder
 
@@ -38,6 +80,8 @@ func main() {
 		email := gofakeit.Email()
 		name := gofakeit.Name()
 		password := gofakeit.Password(true, true, true, true, false, 10)
+
+		password = force_password_pattern(password)
 
 		var row = []string{
 			name, email, password, password,
