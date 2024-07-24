e67c4c81d782acde8d19749bd819ee2334ddfdc2
matheusinit <matheus.oliveira.s@protonmail.com>
2024-07-24 11:24:50 -0300

    feat: make a route to handle the view loading and logic, and another only for api requests

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -23,28 +23,90 @@ class UserController < ApplicationController
     @user = User.new
   end
 
+  def login_api
+    verify_authentication_credentials(email, password)
+
+    # is_email_blank = params[:email].blank?
+    # is_email_nil = params[:email] == nil
+    #
+    # if !is_email_nil and is_email_blank
+    #   # redirect_to login_path, alert: "Endereço de e-mail não informado"
+    #   # return
+    #   head :bad_request
+    # end
+    #
+    # is_valid_email = (params[:email] =~ URI::MailTo::EMAIL_REGEXP) != nil
+    #
+    # unless is_valid_email or is_email_blank
+    #   # redirect_to login_path, alert: "Endereço de e-mail inválido"
+    #   # return
+    #   head :bad_request
+    # end
+    #
+    # is_password_blank = (params[:password] != nil and params[:password].blank?)
+    #
+    # if is_password_blank
+    #   # redirect_to login_path, alert: "Senha não informada"
+    #   # return
+    #   head :bad_request
+    # end
+
+    user = User.find_by(email: params[:email])
+
+    account = user ? Account.find_by(user_id: user.id) : nil
+
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      head :ok
+    elsif not is_email_blank and not is_password_blank
+      login_block_time = 10.minutes
+
+      if account and account.updated_at <= login_block_time.ago
+        reset_login_attempts(user)
+      end
+
+      if user
+        increase_login_attemps(user)
+        login_is_blocked = is_login_blocked?(user)
+      end
+
+      if login_is_blocked
+        # redirect_to login_path, alert: "Muitas tentativas de login, o seu usuario foi bloqueado por 10 minutos. Tente novamente em breve."
+        # return
+        head :forbidden
+      end
+
+      head :unauthorized
+
+      # redirect_to login_path, alert: "Credenciais inválidas"
+    end
+  rescue ActionController::ParameterMissing => _error
+    head :bad_request
+  end
+
   def login
-    is_email_blank = params[:email].blank?
-    is_email_nil = params[:email] == nil
-
-    if !is_email_nil and is_email_blank
-      redirect_to login_path, alert: "Endereço de e-mail não informado"
-      return
-    end
-
-    is_valid_email = (params[:email] =~ URI::MailTo::EMAIL_REGEXP) != nil
-
-    unless is_valid_email or is_email_blank
-      redirect_to login_path, alert: "Endereço de e-mail inválido"
-      return
-    end
-
-    is_password_blank = (params[:password] != nil and params[:password].blank?)
-
-    if is_password_blank
-      redirect_to login_path, alert: "Senha não informada"
-      return
-    end
+    verify_authentication_credentials(email, password)
+    # is_email_blank = params[:email].blank?
+    # is_email_nil = params[:email] == nil
+    #
+    # if !is_email_nil and is_email_blank
+    #   redirect_to login_path, alert: "Endereço de e-mail não informado"
+    #   return
+    # end
+    #
+    # is_valid_email = (params[:email] =~ URI::MailTo::EMAIL_REGEXP) != nil
+    #
+    # unless is_valid_email or is_email_blank
+    #   redirect_to login_path, alert: "Endereço de e-mail inválido"
+    #   return
+    # end
+    #
+    # is_password_blank = (params[:password] != nil and params[:password].blank?)
+    #
+    # if is_password_blank
+    #   redirect_to login_path, alert: "Senha não informada"
+    #   return
+    # end
 
     user = User.find_by(email: params[:email])
 
@@ -72,6 +134,8 @@ class UserController < ApplicationController
 
       redirect_to login_path, alert: "Credenciais inválidas"
     end
+  rescue ActionController::ParameterMissing => error
+    redirect_to login_path, alert: error.message
   end
 
   def destroy_session
@@ -81,6 +145,33 @@ class UserController < ApplicationController
   end
 
   private
+
+  def verify_authentication_credentials(email, password)
+    is_email_blank = params[:email].blank?
+    is_email_nil = params[:email] == nil
+
+    if !is_email_nil and is_email_blank
+      raise ActionController::ParameterMissing("Endereço de e-mail não informado")
+      # redirect_to login_path, alert: "Endereço de e-mail não informado"
+      # return
+    end
+
+    is_valid_email = (params[:email] =~ URI::MailTo::EMAIL_REGEXP) != nil
+
+    unless is_valid_email or is_email_blank
+      # redirect_to login_path, alert: "Endereço de e-mail inválido"
+      # return
+      raise ActionController::ParameterMissing("Endereço de e-mail inválido")
+    end
+
+    is_password_blank = (params[:password] != nil and params[:password].blank?)
+
+    if is_password_blank
+      # redirect_to login_path, alert: "Senha não informada"
+      # return
+      raise ActionController::ParameterMissing("Senha não informada")
+    end
+  end
 
   def increase_login_attemps(user)
     unless user.is_a?(User)
