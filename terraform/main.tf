03c55531c7f8d83889391cbf0464228421a65a25
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-13 10:16:55 -0300

    chore: try to assign a SSH key pair to remote connect to EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_vpc" "app_server_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  instance_tenancy     = "default"
+  enable_dns_support   = true
 
   tags = {
     Name = "app-server"
@@ -24,13 +24,17 @@ resource "aws_vpc" "app_server_vpc" {
 }
 
 resource "aws_subnet" "app_server_subnet" {
-  vpc_id                  = aws_vpc.app_server_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.app_server_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "app-server-subnet"
   }
+}
+
+resource "aws_internet_gateway" "app_server_gw" {
+  vpc_id = aws_vpc.app_server_vpc.id
 }
 
 resource "aws_security_group" "app_server_sg" {
@@ -82,12 +86,31 @@ data "aws_ami" "ubuntu-20-04" {
 }
 
 resource "aws_instance" "app_server" {
-  ami                    = data.aws_ami.ubuntu-20-04.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.app_server_subnet.id
-  vpc_security_group_ids = [aws_security_group.app_server_sg.id]
+  ami                         = data.aws_ami.ubuntu-20-04.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.app_server_subnet.id
+  vpc_security_group_ids      = [aws_security_group.app_server_sg.id]
+  key_name                    = aws_key_pair.key_pair.key_name
+  associate_public_ip_address = true
+  security_groups             = ["${aws_security_group.app_server_sg.id}"]
 
   tags = {
     Name = "app-server"
   }
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${var.namespace}-key"
+  public_key = tls_private_key.app_server_key.public_key_openssh
+}
+
+resource "tls_private_key" "app_server_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  filename        = "${var.namespace}-key.pem"
+  content         = tls_private_key.app_server_key.private_key_pem
+  file_permission = "0400"
 }
