6ffa632f21e7f6de07ac5fcc36d6f2cc25b47665
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-14 22:55:49 -0300

    refactor: rename AWS resources to ones related to the web app

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,56 +13,59 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "app_server_vpc" {
+resource "aws_vpc" "finance_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "app-server"
+    App = "finance-web"
+    Env = "dev"
   }
 }
 
-resource "aws_internet_gateway" "app_server_gw" {
-  vpc_id = aws_vpc.app_server_vpc.id
+resource "aws_internet_gateway" "finance_igw" {
+  vpc_id = aws_vpc.finance_vpc.id
 }
 
-resource "aws_route_table" "app_server_rt" {
-  vpc_id = aws_vpc.app_server_vpc.id
+resource "aws_route_table" "finance_rt" {
+  vpc_id = aws_vpc.finance_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.app_server_gw.id
+    gateway_id = aws_internet_gateway.finance_igw.id
   }
 
   tags = {
-    Name = "public-rts"
+    App = "finance-web"
+    Env = "dev"
   }
 }
 
-resource "aws_subnet" "app_server_subnet" {
-  vpc_id                  = aws_vpc.app_server_vpc.id
+resource "aws_subnet" "finance_vm_public_subnet" {
+  vpc_id                  = aws_vpc.finance_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "app-server-subnet"
+    App = "finance-web"
+    Env = "dev"
   }
 }
 
 resource "aws_route_table_association" "public_rt_assoc" {
-  subnet_id      = aws_subnet.app_server_subnet.id
-  route_table_id = aws_route_table.app_server_rt.id
+  subnet_id      = aws_subnet.finance_vm_public_subnet.id
+  route_table_id = aws_route_table.finance_rt.id
 }
 
 data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
-resource "aws_security_group" "app_server_sg" {
-  name   = "app-server-sg"
-  vpc_id = aws_vpc.app_server_vpc.id
+resource "aws_security_group" "finance_vm_sg" {
+  name   = "finance-vm-sg"
+  vpc_id = aws_vpc.finance_vpc.id
 
   ingress {
     description = "SSH"
@@ -88,7 +91,8 @@ resource "aws_security_group" "app_server_sg" {
   }
 
   tags = {
-    Name = "app-server-sg"
+    App = "finance-web"
+    Env = "dev"
   }
 }
 
@@ -108,33 +112,34 @@ data "aws_ami" "ubuntu-20-04" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "finance_web_vm" {
   ami                         = data.aws_ami.ubuntu-20-04.id
   instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.app_server_subnet.id
-  vpc_security_group_ids      = [aws_security_group.app_server_sg.id]
-  key_name                    = aws_key_pair.key_pair.key_name
+  subnet_id                   = aws_subnet.finance_vm_public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.finance_vm_sg.id]
+  key_name                    = aws_key_pair.ec2_key_pair.key_name
   associate_public_ip_address = true
-  security_groups             = ["${aws_security_group.app_server_sg.id}"]
+  security_groups             = ["${aws_security_group.finance_vm_sg.id}"]
 
   tags = {
-    Name = "app-server"
+    App = "finance-web"
+    Env = "dev"
   }
 }
 
-resource "aws_key_pair" "key_pair" {
+resource "aws_key_pair" "ec2_key_pair" {
   key_name   = "${var.namespace}-key"
-  public_key = tls_private_key.app_server_key.public_key_openssh
+  public_key = tls_private_key.ec2_private_key.public_key_openssh
 }
 
-resource "tls_private_key" "app_server_key" {
+resource "tls_private_key" "ec2_private_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "local_file" "private_key" {
+resource "local_file" "ec2_private_key_local" {
   filename        = "${var.namespace}-key.pem"
-  content         = tls_private_key.app_server_key.private_key_pem
+  content         = tls_private_key.ec2_private_key.private_key_pem
   file_permission = "0400"
 }
 
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,3 @@
-output "instance_ip" {
-  value = aws_instance.app_server.public_ip
+output "vm_instance_ip" {
+  value = aws_instance.finance_web_vm.public_ip
 }
