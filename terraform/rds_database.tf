566e6eb676ead167846e168356426ef09f92198c
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-17 00:19:44 -0300

    refactor: move database provisioning with RDS to its own file

---
+++ terraform/rds_database.tf
@@ -0,0 +1,27 @@
+resource "aws_db_subnet_group" "finance_web_db_subnet_group" {
+  name       = "finance-web-db-subnet-group"
+  subnet_ids = [aws_subnet.finance_db_private_subnet_1.id, aws_subnet.finance_db_private_subnet_2.id]
+
+  tags = {
+    App = "finance-web"
+    Env = "dev"
+  }
+}
+
+resource "aws_db_instance" "finance_web_db" {
+  allocated_storage    = 5
+  instance_class       = "db.t3.micro"
+  engine               = "postgres"
+  engine_version       = "14"
+  username             = var.db_user
+  password             = var.db_password
+  db_name              = "finance_web_db"
+  db_subnet_group_name = aws_db_subnet_group.finance_web_db_subnet_group.name
+  skip_final_snapshot  = true
+
+
+  tags = {
+    App = "finance-web"
+    Env = "dev"
+  }
+}
